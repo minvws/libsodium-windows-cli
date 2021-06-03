23f1f2ffd9be6d14c74d91206942686cd1123785
Ryan Barrett <ryan@seldonplan.com>
2021-06-03 17:16:25 +0200

    Publish with the frameworks, in Portable + Windows 7 for both 32/64bit versions

--- publish.bat
+++ publish.bat
@@ -1,1 +1,4 @@
-dotnet publish src/NaCLI/NaCLI.csproj -o publish
+dotnet publish src/NaCLI/NaCLI.csproj --self-contained -r win-x64 -o publish/win-x64
+dotnet publish src/NaCLI/NaCLI.csproj --self-contained -r win-x86 -o publish/win-x86
+dotnet publish src/NaCLI/NaCLI.csproj --self-contained -r win7-x64 -o publish/win7-x64
+dotnet publish src/NaCLI/NaCLI.csproj --self-contained -r win7-x86 -o publish/win7-x86
